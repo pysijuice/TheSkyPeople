b1ff2a9d09b0a65f80fe91c5c17b7982c12016d3
Spazrael <daniil127@mail.ru>
2022-04-26 17:52:22 +0300

    NewEraWithKorablik

---
+++ Assets/Scripts/Ship Controller/AirshipTest.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ditzelgames;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class AirshipTest : MonoBehaviour
+{
+
+    //visible Properties
+    public float rotationSpeed;
+    public Transform motor; 
+    public float SteerPower = 500f;
+    public float Power = 5f;
+    public float MaxSpeed = 10f;
+    public float crenCoef;
+    public float upDownCoef;
+    public float passiveCrenCoef;
+
+
+
+
+
+    private float moveForward = 0;
+
+    //use Componets
+    protected Rigidbody Rigidbody;
+    protected Quaternion StartRotation;
+    float steer;
+    float verticalInput;
+    float upDownInput;
+    float brakeInput;
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        StartRotation = motor.localRotation;
+        Transform zepTransform = gameObject.GetComponent<Transform>(); 
+        Physics.gravity = new Vector3(0,0,0);
+        
+    }
+
+    void Update(){
+        upDownInput = Input.GetAxis("ShipUp");
+        steer = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        brakeInput = Input.GetAxis("Jump");
+        
+
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (Input.GetAxisRaw("Vertical")>0){
+                moveForward += 0.25f;
+                if (moveForward > 1)
+                {
+                    moveForward = 1;
+                } 
+                Debug.Log(moveForward);
+            }
+            else{
+                moveForward -= 0.25f;
+                if (moveForward < -1)
+                {
+                    moveForward = -1;
+                } 
+                Debug.Log(moveForward);
+            }
+        }
+    }
+    void FixedUpdate()
+    {   
+        //default direction
+        var forceDirection = transform.forward;
+        var forward = Vector3.Scale(new Vector3(1,1,1), -transform.right);
+        //upDown
+        transform.Rotate(new Vector3(0,0,Time.deltaTime*upDownInput*100/upDownCoef),Space.Self);
+        
+        // //leftRight
+
+        Rigidbody.AddForceAtPosition(-Time.deltaTime*steer * transform.forward * SteerPower / 100f, motor.position);
+        Rigidbody.AddForce(transform.forward*steer*2, ForceMode.Force);
+        Rigidbody.AddForceAtPosition(Time.deltaTime*steer * transform.forward * SteerPower / 200f, motor.position, ForceMode.Force);
+
+        //forwardBack
+        PhysicsHelper.ApplyForceToReachVelocity(Rigidbody, forward * MaxSpeed * moveForward, Power);
+
+        //brake
+        Rigidbody.AddForce(-brakeInput*5 * Rigidbody.velocity);
+        Rigidbody.AddForce(-0.7f * Rigidbody.velocity);
+
+        if (transform.eulerAngles.x <180){
+            transform.Rotate(transform.right * rotationSpeed * Time.deltaTime  * -1 * passiveCrenCoef,Space.World);
+        } else if (transform.eulerAngles.x > 180){
+            transform.Rotate(transform.right * rotationSpeed * Time.deltaTime * passiveCrenCoef, Space.World);
+           
+        }
+        
+    }
+   
+}
