de4e7a7c385dc9470bfe381a31a31b11612cccea
Dmitry Leonenko <elspark10101@gmail.com>
2022-05-11 21:36:30 +0700

    added new simple UI

---
+++ Assets/Scripts/OtherScripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text levelNameText;
+    private float alpha = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelNameText.alpha = 0f;
+        StartCoroutine(ChangeLevelNameText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator ChangeLevelNameText()
+    {
+        while (alpha <= 1)
+        {
+            alpha += 0.01f;
+            levelNameText.alpha = alpha;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        while (alpha != 0)
+        {
+            alpha -= 0.01f;
+            levelNameText.alpha = alpha;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
